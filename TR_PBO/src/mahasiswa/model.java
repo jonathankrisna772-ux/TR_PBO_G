472bd7d53741c839efc9748323c5e96709767174
jonathankrisna772-ux <jonathankrisna772@gmail.com>
2025-11-27 09:16:18 +0700

    Update model.java

--- TR_PBO/src/mahasiswa/model.java
+++ TR_PBO/src/mahasiswa/model.java
@@ -1,8 +1,70 @@
 package mahasiswa;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-
 public class model {
-    private int 
+    private String nim;
+    private int userId;
+    private int prodiId;
+    private String nama;
+    private String email;
+    private int angkatan;
+    
+public model(String nim,int userId, int prodiId, String nama, String email, int angkatan){
+this.nim = nim;
+this.userId = userId;
+this.prodiId = prodiId;
+this.nama = nama;
+this.email = email;
+this.angkatan = angkatan;
+
+}
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getProdiId() {
+        return prodiId;
+    }
+
+    public void setProdiId(int prodiId) {
+        this.prodiId = prodiId;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAngkatan() {
+        return angkatan;
+    }
+
+    public void setAngkatan(int angkatan) {
+        this.angkatan = angkatan;
+    }
+
+    
 }
