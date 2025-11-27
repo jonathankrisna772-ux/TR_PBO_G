7b4a7381fa4d12918eea0b9416ca0c3a34f87c41
jonathankrisna772-ux <jonathankrisna772@gmail.com>
2025-11-27 09:16:33 +0700

    Create model_users.java

---
+++ TR_PBO/src/users/model_users.java
@@ -0,0 +1,49 @@
+package users;
+
+public class model_users {
+    private int user_id;
+    private String username;
+    private String password;
+    private String role;
+    
+public model_users(int user_id,String username,String password, String role){
+this.user_id = user_id;
+this.username = username;
+this.password = password;
+this.role = role;
+}
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    
+}
